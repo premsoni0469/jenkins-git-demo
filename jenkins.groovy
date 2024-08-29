d3cba31a687a8d9755a37f348e1d2b032f92b704
Prem Soni <premsoni0469@gmail.com>
2024-08-29 12:04:09 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -17,19 +17,18 @@ pipeline {
                 echo 'Deployment will be done on my Birthday!!'
             }
         }
-    }
-}
-stage('SonarQube analysis') {
-    steps {
-        withSonarQubeEnv('SonarHome')
-        {
-            bat script: """
-            sonar-scanner -D"sonar.projectKey=java" \
-            -D"sonar.sources=." \
-            -D"sonar.host.url=http://localhost:9000" \
-            -D"sonar.login=sqa_6b6ce34165439e19db2e851347e89ca00c870a85"
-            """
+        stage('SonarQube analysis') {
+            steps {
+                withSonarQubeEnv('SonarHome')
+                {
+                    bat script: """
+                    sonar-scanner -D"sonar.projectKey=java" \
+                    -D"sonar.sources=." \
+                    -D"sonar.host.url=http://localhost:9000" \
+                    -D"sonar.login=sqa_6b6ce34165439e19db2e851347e89ca00c870a85"
+                    """
+                }
+            }
         }
     }
 }
-
