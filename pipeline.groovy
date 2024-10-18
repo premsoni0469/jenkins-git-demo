f064cb856b8ce8b46418df5fc11cd2e969bcecd1
Prem Soni <premsoni0469@gmail.com>
2024-10-18 23:51:14 +0530

    Create pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deployment will be done on my Birthday!!'
+            }
+        }
+    }
+}
