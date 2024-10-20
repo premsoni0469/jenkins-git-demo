86d63b622ba80be5e15ad455a5111317ad8bc994
Prem Soni <premsoni0469@gmail.com>
2024-10-20 13:53:46 +0530

    Update java-pipeline.groovy

--- java-pipeline.groovy
+++ java-pipeline.groovy
@@ -5,14 +5,14 @@ pipeline {
         stage('Compile Java Program') {
             steps {
                 // Compile the Java program from the JenkinsPipeline folder
-                bat 'javac C:\\JenkinsPipeline\\Test2.java'
+                bat 'javac C:\\JenkinsPipeline\\Test1.java'
             }
         }
         stage('Run Java Program') {
             steps {
                 // Run the Java program from the JenkinsPipeline folder
                 // -cp = class path: used to execute the class file for the compiled java file
-                bat 'java -cp C:\\JenkinsPipeline Test2'
+                bat 'java -cp C:\\JenkinsPipeline Test1'
             }
         }
     }
